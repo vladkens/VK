f729f0f886d8923a5629127ee940f9c69f7988e4
Vlad Pronsky <vladkens@yandex.ru>
2015-06-15 22:21:49 +0300

    Update VK.php

--- src/VK/VK.php
+++ src/VK/VK.php
@@ -4,7 +4,6 @@
  * The PHP class for vk.com API and to support OAuth.
  * @author Vlad Pronsky <vladkens@yandex.ru>
  * @license https://raw.github.com/vladkens/VK/master/LICENSE MIT
- * @version 0.1.6
  */
 
 namespace VK;
