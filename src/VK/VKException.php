88bdf9f2fd1f603e37cebd2fb44ad1d97bc40182
Vlad Pronsky <vladkens@yandex.ru>
2015-06-15 22:22:03 +0300

    Update VKException.php

--- src/VK/VKException.php
+++ src/VK/VKException.php
@@ -4,7 +4,6 @@
  * The exception class for VK library.
  * @author Vlad Pronsky <vladkens@yandex.ru>
  * @license https://raw.github.com/vladkens/VK/master/LICENSE MIT
- * @version 0.1.6
  */
 
 namespace VK;
